66d56bce198bef048ae6f4bd4b75f5d95d9ae00b
tech-arichan <arihiro2940@gmail.com>
2021-01-15 14:46:46 +0900

    詳細表示機能（予定内容確認）

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:show]
+
   def index
     @events = Event.all
   end
@@ -12,10 +14,17 @@ class EventsController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+  end
+
   private
 
   def event_parameter
     params.require(:event).permit(:title, :content, :start_time).merge(user_id: current_user.id)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
   end
 
 end
