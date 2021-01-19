59361ebabdb2505f96b13183ef05e81d2cb214eb
tech-arichan <arihiro2940@gmail.com>
2021-01-19 13:57:06 +0900

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_15_065447) do
+ActiveRecord::Schema.define(version: 2021_01_19_040843) do
 
   create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
