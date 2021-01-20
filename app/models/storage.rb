776c04236079783490086fffccd72e2762ca8cea
tech-arichan <arihiro2940@gmail.com>
2021-01-20 16:40:39 +0900

    view修正

--- app/models/storage.rb
+++ app/models/storage.rb
@@ -1,5 +1,10 @@
 class Storage < ApplicationRecord
   belongs_to :user
-  validates :title, presence: true
   has_one_attached :image
+
+  validates :title, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
 end
