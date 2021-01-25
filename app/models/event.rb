29dbf62533f3b4dcf1f07bd057f4d36321bfc7c2
tech-arichan <arihiro2940@gmail.com>
2021-01-26 08:56:10 +0900

    Rspec導入　設定

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :user
-  validates :title, presence: true
-  validates :content, presence: true
+  
+  with_options presence: true do
+    validates :title
+    validates :content
+  end
 end
