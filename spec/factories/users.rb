1aebe16354266e62144f6dc28587f14811ec7691
tech-arichan <arihiro2940@gmail.com>
2021-01-29 17:06:44 +0900

    ユーザー結合テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    name         {Faker::Name.first_name  }
+    name         {Faker::Name.first_name}
     email        {Faker::Internet.free_email}
     password     {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
