d80ec13c0f2c67b92ea1223b62d189def14aa104
tech-arichan <arihiro2940@gmail.com>
2021-02-14 14:53:07 +0900

    メモ機能　view改善、メモ機能new.html.erb削除

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,19 +1,13 @@
 class MemosController < ApplicationController
   def index
     @memos = Memo.includes(:user)
-  end
-
-  def new
     @memo = Memo.new
   end
 
   def create
     @memo = Memo.new(memo_params)
-    if @memo.save
-      redirect_to memos_path
-    else
-      redirect_to new_memo_path
-    end
+    @memo.save
+    redirect_to memos_path
   end
 
 
